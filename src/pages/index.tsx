3fa211d9bbff93fa199463ab0a9b048cbc85690e
Katleho Janco <katlehomabala3@gmail.com>
2023-04-29 21:58:12 +0200

    feat - added dummy data to table & completed it.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
-// import { Inter } from 'next/font/google'
+
 import Navbar from '../../components/Navbar'
 
-// const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -16,34 +16,48 @@ export default function Projects() {
           <section className="my-4">
             <InputField className="searchInput input" name="search" type="search" placeholder="Search here" />
           </section>
-          <section className="w-full overflow-x-auto">
-            <table className="mt-4 border-collapse table-auto">
+          <section className="w-full overflow-auto">
+            <table className="mt-4 border-collapse table-auto w-full">
               <thead className="bg-gray-100 text-gray-800">
                 <tr>
+                  <th className="text-gray-800 font-sans font-semibold text-center">Service Order No.</th>
+                  <th className="text-gray-800 font-sans font-semibold text-center">Ticket No.</th>
                   <th className="text-gray-800 font-sans font-semibold text-center">Unit Model</th>
                   <th className="text-gray-800 font-sans font-semibold text-center">Unit Problem</th>
+                  <th className="text-gray-800 font-sans font-semibold text-center">In/Out Warranty</th>
                   <th className="text-gray-800 font-sans font-semibold text-center">Category</th>
+                  <th className="text-gray-800 font-sans font-semibold text-center">Booking Date</th>
                   <th className="text-gray-800 font-sans font-semibold text-center">Date Assessed</th>
                   <th className="text-gray-800 font-sans font-semibold text-center">Assessment Completion</th>
+                  <th className="text-gray-800 font-sans font-semibold text-center">Technician</th>
+                  <th className="text-gray-800 font-sans font-semibold text-center">Parts Ordered Date</th>
+                  <th className="text-gray-800 font-sans font-semibold text-center">Parts ETA</th>
                   <th className="text-gray-800 font-sans font-semibold text-center">Repair Date</th>
                   <th className="text-gray-800 font-sans font-semibold text-center">Repair Completed</th>
+                  <th className="text-gray-800 font-sans font-semibold text-center">Repair Period</th>
                   <th className="text-gray-800 font-sans font-semibold text-center">Status</th>
-                  <th className="text-gray-800 font-sans font-semibold text-center">Technician</th>
                 </tr>
               </thead>
               <tbody>
                 {
                   tableData.map((item) => (
                     <tr key={item.id}>
+                      <td className="text-gray-800 font-sans font-normal text-center">{item.serviceOrder}</td>
+                      <td className="text-gray-800 font-sans font-normal text-center">{item.tickeNo}</td>
                       <td className="text-gray-800 font-sans font-normal text-center">{item.model}</td>
                       <td className="text-gray-800 font-sans font-normal text-center">{item.problem}</td>
+                      <td className="text-gray-800 font-sans font-normal text-center">{item.warranty}</td>
                       <td className="text-gray-800 font-sans font-normal text-center">{item.category}</td>
+                      <td className="text-gray-800 font-sans font-normal text-center">{item.bookingDate}</td>
                       <td className="text-gray-800 font-sans font-normal text-center">{item.assessDate}</td>
                       <td className="text-gray-800 font-sans font-normal text-center">{item.assessComplete}</td>
+                      <td className="text-gray-800 font-sans font-normal text-center">{item.tech}</td>
+                      <td className="text-gray-800 font-sans font-normal text-center">{item.partsOrdered}</td>
+                      <td className="text-gray-800 font-sans font-normal text-center">{item.partsETA}</td>
                       <td className="text-gray-800 font-sans font-normal text-center">{item.repairDate}</td>
                       <td className="text-gray-800 font-sans font-normal text-center">{item.repairComplete}</td>
+                      <td className="text-gray-800 font-sans font-normal text-center">{item.repairPeriod}</td>
                       <td className="text-gray-800 font-sans font-normal text-center">{item.status}</td>
-                      <td className="text-gray-800 font-sans font-normal text-center">{item.tech}</td>
                     </tr>
                   ))
                 }
