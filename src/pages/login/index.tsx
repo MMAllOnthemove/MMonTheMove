43f602282717b84e1112338c45d1dd77132c7817
Katleho <katlehomabala3@gmail.com>
2023-05-02 09:30:50 +0200

    bug - added &apos; to replace single quotes

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -64,7 +64,7 @@ export default function Login() {
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <Link href="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
               Signup
             </Link>
