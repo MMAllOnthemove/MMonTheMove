7bc73aa2216c45e46fb04562055b8234d2ef02fb
Katleho <katlehomabala3@gmail.com>
2023-05-05 14:13:41 +0200

    I will come back to parts soon...

--- src/pages/parts/index.tsx
+++ src/pages/parts/index.tsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import Navbar from "../../../components/Navbar";
 
 function Parts() {
-  const [data, setData] = useState<null | any>(null);
+  // const [data, setData] = useState([]);
+  const [Description, setDescription] = useState("");
   const [isLoading, setLoading] = useState(false);
-  const [search, setSearch] = useState<string | any>("");
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     async function getData(url = "", data = {}) {
@@ -24,9 +25,13 @@ function Parts() {
         body: JSON.stringify(data), // body data type must match "Content-Type" header
       })
         .then((res) => res.json())
-        .then((data: any) => {
-          // console.log(data);
-          setData(data);
+        .then((data) => {
+          // console.log(data.Return.EsPartsInfo);
+          // let obj = data.Return.EsPartsInfo;
+          // let arr = (Object.values(obj))
+          // console.log(arr);
+         
+          // setData(data);
           setLoading(false);
         });
     }
@@ -39,84 +44,94 @@ function Parts() {
         Country: "ZA",
         Pac: "999999920180502152320",
       },
-      IvPartsNo: search,
+      IvPartsNo: "DC66-00010A",
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
+  // if (isLoading) return <p>Loading...</p>;
+  // if (!data) return <p>No profile data</p>;
 
-const PartInfo = (props:any)=>
-props.list.map((item:any) => (
-  <span key={item.Description}>
-    <p>{item.ASCPrice}</p>
-    <p>{item.Color}</p>
-    <p>{item.CoreAPrice}</p>
-    <p>{item.CoreBPrice}</p>
-    <p>{item.Currency}</p>
-    <p>{item.Division}</p>
-    <p>{item.DivisionDesc}</p>
-    <p>{item.DivisionName}</p>
-    <p>{item.PartsDescription}</p>
-    <p>{item.PartsNo}</p>
-    <p>{item.RetailPrice}</p>
-    <p>{item.SalesStatus}</p>
-    <p>{item.StockAvalability}</p>
-    <p>{item.UnitPrice}</p>
-  </span>
-))
-const handleSearch = (event: any) => {
-  setSearch(event.target.value);
-};
-const filterPartInfo = Object.values(data.Return).filter((part: any) => {
+  const PartInfoList = (props) =>
+    props.list.map((part, index) => {
+      const {
+        PartsNo,
+        Division,
+        DivisionDesc,
+        PartsDescription,
+        SalesStatus,
+        StockAvalability,
+        UnitPrice,
+        Currency,
+        Color,
+        DivisionName,
+        RetailPrice,
+        ASCPrice,
+        CoreAPrice,
+        CoreBPrice,
+      } = part;
+
+      return (
+        <tr key={index}>
+          <td>{PartsNo}</td>
+          <td>{Division}</td>
+          <td>{DivisionDesc}</td>
+          <td>{PartsDescription}</td>
+          <td>{SalesStatus}</td>
+          <td>{StockAvalability}</td>
+          <td>{UnitPrice}</td>
+          <td>{Currency}</td>
+          <td>{Color}</td>
+          <td>{DivisionName}</td>
+          <td>{RetailPrice}</td>
+          <td>{ASCPrice}</td>
+          <td>{CoreAPrice}</td>
+          <td>{CoreBPrice}</td>
+        </tr>
+      );
+    });
+    const handleSubmit = (e)=>{
+      e.preventDefault(); 
+      setSearch(search);
+      
+    }
+    const handleSearch = (event) => {
+      setSearch(event.target.value);
+    };
+ 
   return (
-    part.Description.includes(search)
-  );
-});
-  return (
-    <>
-      <Navbar />
+   <>
+    <Navbar />
       <main>
         <div className="container mx-auto p-4">
           <section className="my-4 flex justify-center items-center gap-2">
-            <h2></h2>
-            <form>
+            <h2>Search Part</h2>
+           
               <input
                 className="searchInput input placeholder-sky-900 outline-none text-gray-950"
                 placeholder="Search here"
                 type="text"
                 value={search}
-                onChange={(e: any) => setSearch(e.target.value)}
+                onChange={handleSearch}
+                onKeyDown={event => {
+                  if(event.key === "Enter"){
+                    console.log("Pressed")
+                  }
+                }}
               />
-            </form>
+        
 
-            <button className="bg-sky-700 py-3 px-4 rounded text-white border border-sky-700 font-sans font-medium hover:bg-sky-950" value={search} onChange={handleSearch}>
-              Search{" "}
-            </button>
+            <input type="submit" value="Search" onClick={handleSubmit}
+              className="bg-sky-700 py-3 px-4 rounded text-white border border-sky-700 font-sans font-medium hover:bg-sky-950"
+            />
+            
           </section>
 
-          {/* {Object.values(data.Return).map((item: any) => (
-            <span key={item.Description}>
-              <p>{item.ASCPrice}</p>
-              <p>{item.Color}</p>
-              <p>{item.CoreAPrice}</p>
-              <p>{item.CoreBPrice}</p>
-              <p>{item.Currency}</p>
-              <p>{item.Division}</p>
-              <p>{item.DivisionDesc}</p>
-              <p>{item.DivisionName}</p>
-              <p>{item.PartsDescription}</p>
-              <p>{item.PartsNo}</p>
-              <p>{item.RetailPrice}</p>
-              <p>{item.SalesStatus}</p>
-              <p>{item.StockAvalability}</p>
-              <p>{item.UnitPrice}</p>
-            </span>
-          ))} */}
-              <PartInfo list={filterPartInfo} />
+        <section>
+        {/* <PartInfoList list={filterPartInfo} /> */}
+        </section>
         </div>
       </main>
-    </>
+   </>
   );
 }
 
