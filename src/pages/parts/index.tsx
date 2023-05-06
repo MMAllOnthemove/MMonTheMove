3361800c4bb72f20238012ba2eee5607448bd5a1
Katleho Janco <katlehomabala3@gmail.com>
2023-05-06 19:47:29 +0200

    tried the search again, think the network is slow

--- src/pages/parts/index.tsx
+++ src/pages/parts/index.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
-import Navbar from "../../../components/Navbar";
+import React, { useState, useEffect } from 'react'
+import Navbar from '../../../components/Navbar';
 
-export default function Parts() {
+function Parts() {
   const [data, setData] = useState<null | any>(null);
   const [Description, setDescription] = useState<string>("");
   const [isLoading, setLoading] = useState(false);
@@ -25,8 +25,8 @@ export default function Parts() {
         body: JSON.stringify(data), // body data type must match "Content-Type" header
       })
         .then((res) => res.json())
-        .then((data: any) => {
-          console.log(data.Return.EsPartsInfo);
+        .then((data: string | any) => {
+          console.log(data.Return.EsPartsInfo.PartsDescription);
           //   console.log(Object.values(data.Return.EsPartsInfo));
           //   console.log(Object.values(data.Return.EsPartsInfo));
           setData(data);
@@ -46,47 +46,8 @@ export default function Parts() {
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [search]);
-  // if (isLoading) return <p>Loading...</p>;
-  // if (!data) return <p>No profile data</p>;
 
-  const PartInfoList = (props: any) =>
-    props.list.map((part: any, index: any) => {
-      const {
-        PartsNo,
-        Division,
-        DivisionDesc,
-        PartsDescription,
-        SalesStatus,
-        StockAvalability,
-        UnitPrice,
-        Currency,
-        Color,
-        DivisionName,
-        RetailPrice,
-        ASCPrice,
-        CoreAPrice,
-        CoreBPrice,
-      } = part;
 
-      return (
-        <tr key={index}>
-          <td>{PartsNo}</td>
-          <td>{Division}</td>
-          <td>{DivisionDesc}</td>
-          <td>{PartsDescription}</td>
-          <td>{SalesStatus}</td>
-          <td>{StockAvalability}</td>
-          <td>{UnitPrice}</td>
-          <td>{Currency}</td>
-          <td>{Color}</td>
-          <td>{DivisionName}</td>
-          <td>{RetailPrice}</td>
-          <td>{ASCPrice}</td>
-          <td>{CoreAPrice}</td>
-          <td>{CoreBPrice}</td>
-        </tr>
-      );
-    });
   const handleSubmit = (e: any) => {
     e.preventDefault();
     setSearch(search);
@@ -95,10 +56,9 @@ export default function Parts() {
   const handleSearch = (event: any) => {
     setSearch(event.target.value);
   };
-
   return (
-    <>
-      <Navbar />
+    <div>
+      <Navbar/>
       <main>
         <div className="container mx-auto p-4">
           <section className="my-4 flex justify-center items-center gap-2">
@@ -120,7 +80,6 @@ export default function Parts() {
               className="bg-sky-700 py-3 px-4 rounded text-white border border-sky-700 font-sans font-medium hover:bg-sky-950"
             />
           </section>
-
           {/* {Object.values(data.Return.EsPartsInfo)} */}
           <div className="w-100 overflow-auto">
             <table className="table table-hoverable border-collapse font-weight-normal">
@@ -163,9 +122,10 @@ export default function Parts() {
                 {/* data.EsPartsInfo)} */}
               </tbody>
             </table>
-          </div>
         </div>
       </main>
-    </>
-  );
+    </div>
+  )
 }
+
+export default Parts
