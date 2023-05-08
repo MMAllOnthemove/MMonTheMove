898b9fcb9b7c3a0b583ebe8b5f06760589e8ac1b
Katleho <katlehomabala3@gmail.com>
2023-05-08 10:06:30 +0200

    fix - changed projects to management page

--- public/_data/navbar.ts
+++ public/_data/navbar.ts
@@ -1,5 +1,5 @@
 export const navitems = [
-  { id: 1, item: "Projects", pageRoute: "/projects" },
+  { id: 1, item: "Management", pageRoute: "/management" },
   { id: 2, item: "Dashboard", pageRoute: "/dashboard" },
   { id: 3, item: "Orders", pageRoute: "/orders" },
   { id: 4, item: "Notifications", pageRoute: "/notifications" },
