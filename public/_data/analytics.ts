77ff74f0f6358eafff5ac76999e5841c9d29e986
Katleho <katlehomabala3@gmail.com>
2023-05-08 12:46:00 +0200

    feat - created units_in page

--- public/_data/analytics.ts
+++ public/_data/analytics.ts
@@ -3,20 +3,27 @@ export const analytic = [
     id: 1,
     title: "Units in",
     stat: 100,
+    href: "/units_in"
   },
   {
     id: 2,
     title: "Assessed",
     stat: 30,
+    href: "/units_assessed"
+
   },
   {
     id: 3,
     title: "Pending",
     stat: 34,
+    href: "/units_pending"
+
   },
   {
     id: 4,
     title: "Complete",
     stat: 60,
+    href: "/units_complete"
+
   },
 ];
---
+++ src/pages/dashboard/units_in/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../../../components/Navbar'
+
+export default function Units_in() {
+  return (
+    <>
+    <Navbar/>
+    <main>
+    <section className='department_cards'>
+        
+                    </section>
+    </main>
+    </>
+  )
+}
