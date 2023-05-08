7727fd5070e140e9c86a297f2ad290eba21a891a
Katleho <katlehomabala3@gmail.com>
2023-05-08 10:50:06 +0200

    moved hhp folder inside dashboard folder

---
+++ src/pages/dashboard/hhp/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../../../../components/Navbar";
+
+function UnitsIn() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        
+      </main>
+    </>
+  );
+}
+
+export default UnitsIn;
