f01dc613923fecbc42e758646514ed66ad3065f1
Katleho <katlehomabala3@gmail.com>
2023-05-08 16:26:47 +0200

    fix - can now use button after searching in parts

--- src/pages/parts/index.tsx
+++ src/pages/parts/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import Navbar from '../../../components/Navbar';
+import React, { useState, useEffect } from "react";
+import Navbar from "../../../components/Navbar";
 
 function Parts() {
   const [data, setData] = useState<null | any>(null);
@@ -47,13 +47,13 @@ function Parts() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [search]);
 
-
   // const handleSubmit = (e: any) => {
   //   e.preventDefault();
   //   setSearch(search);
   //   // console.log("It works", search);
   // };
-  const handleSearch = (event: any) => {
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
     setSearch(event.target.value);
   };
   return (
@@ -61,48 +61,86 @@ function Parts() {
       <Navbar />
       <main>
         <div className="container mx-auto p-4">
-          <section className="my-4 flex justify-center items-center gap-2">
-            <h2></h2>
-            <form>
-              <label htmlFor="searchPart" className="sr-only">Search Part</label>
+          <section className="my-4 flex justify-center flex-col items-center gap-2">
+            <form onSubmit={handleSubmit} className="flex flex-row gap-2">
+              <label htmlFor="searchPart" className="sr-only">
+                Search Part
+              </label>
               <input
                 className="searchInput input placeholder-sky-900 outline-none text-gray-950"
-                placeholder="Search here"
+                placeholder="Search Part"
                 type="text"
                 value={search}
-                onChange={handleSearch}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <input
+                type="submit"
+                value="Search"
+                className="bg-sky-700 py-3 px-4 rounded text-white border-0 font-sans font-medium hover:bg-sky-950"
               />
             </form>
-
-            {/* <input
-              type="submit"
-              value="Search"
-              onClick={handleSubmit}
-              className="bg-sky-700 py-3 px-4 rounded text-white border border-sky-700 font-sans font-medium hover:bg-sky-950"
-            /> */}
           </section>
           <section className="flex justify-center mx-auto p-2">
-            <article className="part_info_card">
-              <p>Part No: <span>{data?.Return.EsPartsInfo.PartsNo}</span></p>
-              <p>Part Name: <span>{data?.Return.EsPartsInfo.PartsDescription}</span></p>
-              <p>Part Division: <span>{data?.Return.EsPartsInfo.Division}</span></p>
-              <p>Division Name: <span>{data?.Return.EsPartsInfo.DivisionDesc}</span></p>
-              <p>Sales Status: <span>{data?.Return.EsPartsInfo.SalesStatus}</span></p>
-              <p>Stock Availability: <span>{data?.Return.EsPartsInfo.StockAvailability}</span></p>
-              <p>Unit Price: <span>{data?.Return.EsPartsInfo.UnitPrice}</span></p>
-              <p>Currency: <span>{data?.Return.EsPartsInfo.Currency}</span></p>
-              <p>Color: <span>{data?.Return.EsPartsInfo.Color}</span></p>
-              <p>Division Name: <span>{data?.Return.EsPartsInfo.DivisionName}</span></p>
-              <p>Retail Price: <span>{data?.Return.EsPartsInfo.RetailPrice}</span></p>
-              <p>ASC Price: <span>{data?.Return.EsPartsInfo.ASCPrice}</span></p>
-              <p>Core A Price: <span>{data?.Return.EsPartsInfo.CoreAPrice}</span></p>
-              <p>Core B Price: <span>{data?.Return.EsPartsInfo.CoreBPrice}</span></p>
-            </article>
+           
+              <article className="part_info_card">
+                <p>
+                  Part No: <span>{data?.Return.EsPartsInfo.PartsNo}</span>
+                </p>
+                <p>
+                  Part Name:{" "}
+                  <span>{data?.Return.EsPartsInfo.PartsDescription}</span>
+                </p>
+                <p>
+                  Part Division:{" "}
+                  <span>{data?.Return.EsPartsInfo.Division}</span>
+                </p>
+                <p>
+                  Division Name:{" "}
+                  <span>{data?.Return.EsPartsInfo.DivisionDesc}</span>
+                </p>
+                <p>
+                  Sales Status:{" "}
+                  <span>{data?.Return.EsPartsInfo.SalesStatus}</span>
+                </p>
+                <p>
+                  Stock Availability:{" "}
+                  <span>{data?.Return.EsPartsInfo.StockAvailability}</span>
+                </p>
+                <p>
+                  Unit Price: <span>{data?.Return.EsPartsInfo.UnitPrice}</span>
+                </p>
+                <p>
+                  Currency: <span>{data?.Return.EsPartsInfo.Currency}</span>
+                </p>
+                <p>
+                  Color: <span>{data?.Return.EsPartsInfo.Color}</span>
+                </p>
+                <p>
+                  Division Name:{" "}
+                  <span>{data?.Return.EsPartsInfo.DivisionName}</span>
+                </p>
+                <p>
+                  Retail Price:{" "}
+                  <span>{data?.Return.EsPartsInfo.RetailPrice}</span>
+                </p>
+                <p>
+                  ASC Price: <span>{data?.Return.EsPartsInfo.ASCPrice}</span>
+                </p>
+                <p>
+                  Core A Price:{" "}
+                  <span>{data?.Return.EsPartsInfo.CoreAPrice}</span>
+                </p>
+                <p>
+                  Core B Price:{" "}
+                  <span>{data?.Return.EsPartsInfo.CoreBPrice}</span>
+                </p>
+              </article>
+          
           </section>
         </div>
       </main>
     </>
-  )
+  );
 }
 
-export default Parts
+export default Parts;
