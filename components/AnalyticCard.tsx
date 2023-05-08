fce2458478daa26c1cb0288f8ea45e863dcda49b
Katleho <katlehomabala3@gmail.com>
2023-05-08 11:01:14 +0200

    style - added cursor-pointer to dashboard card

--- components/AnalyticCard.tsx
+++ components/AnalyticCard.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { analytic } from '../public/_data/analytics'
 
+
 function AnalyticCard() {
   return (
     <>
 
       {
         analytic.map((item) => (
-          <article className='bg-white border-t-4 border-sky-600 p-8 shadow-md rounded grid text-center' key={item.id}>
+          <article className='bg-white border-t-4 border-sky-600 p-8 shadow-md rounded grid text-center cursor-pointer' key={item.id}>
             <div className="flex gap-2 flex-col">
               <h2 className="font-sans text-5xl font-semibold text-gray-900">{item.stat}</h2>
               <p className="font-sans tracking-wide text-xl text-gray-600 uppercase">{item.title}</p>
