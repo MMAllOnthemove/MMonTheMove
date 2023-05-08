27a7112bb546857a8fa4e802225c46e5829e95f9
Katleho <katlehomabala3@gmail.com>
2023-05-08 13:43:11 +0200

    feat - added a units_in page showing departments

---
+++ public/_data/department_card.ts
@@ -0,0 +1,26 @@
+export const department_card = [
+    {
+        id: 1,
+        title: "HHP",
+        typeOfCard: "UNITS IN",
+        href: "/dashboard/units_in/hhp"
+    },
+    {
+        id: 2,  
+        title: "DTV",
+        typeOfCard: "UNITS IN",
+        href: "/dashboard/units_in/dtv"
+    },
+    {
+        id: 3,
+        title: "HA",
+        typeOfCard: "UNITS IN",
+        href: "/dashboard/units_in/ha"
+    },
+    {
+        id: 4,
+        title: "In Home",
+        typeOfCard: "UNITS IN",
+        href: "/dashboard/units_in/in_home"
+    }
+]
