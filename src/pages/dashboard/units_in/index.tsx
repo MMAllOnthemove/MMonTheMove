b780f34653710dd2a195014d31697de3eef2e1c6
Katleho <katlehomabala3@gmail.com>
2023-05-08 13:43:11 +0200

    feat - added a units_in page showing departments

--- src/pages/dashboard/units_in/index.tsx
+++ src/pages/dashboard/units_in/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import Navbar from '../../../../components/Navbar'
+import React from "react";
+import Navbar from "../../../../components/Navbar";
+import DepartmentCard from "../../../../components/DepartmentCard";
 
 export default function Units_in() {
   return (
     <>
-    <Navbar/>
-    <main>
-    <section className='department_cards'>
-        
-                    </section>
-    </main>
+      <Navbar />
+      <main>
+        <div className="container mx-auto p-2">
+          <section className="department_cards">
+            <DepartmentCard />
+          </section>
+        </div>
+      </main>
     </>
-  )
+  );
 }
