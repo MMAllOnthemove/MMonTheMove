9db31155e946fdd6f13fbc102c99e1ad854e888e
Katleho <katlehomabala3@gmail.com>
2023-05-10 10:39:55 +0200

    feat - managed to use fetch in management screen

--- src/pages/management/index.tsx
+++ src/pages/management/index.tsx
@@ -1,11 +1,55 @@
 import Navbar from "../../../components/Navbar";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 
 export default function Management() {
   const [data, setData] = useState<null | any>(null);
-
+  const [isLoading, setLoading] = useState(false);
   const [search, setSearch] = useState<string>("");
 
+
+
+  // useEffect(() => {
+  //   async function getData(url = "", data = {}) {
+  //     setLoading(true);
+  //     await fetch(url, {
+  //       method: "POST", // *GET, POST, PUT, DELETE, etc.
+  //       mode: "cors", // no-cors, *cors, same-origin
+  //       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+  //       credentials: "same-origin", // include, *same-origin, omit
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: "Bearer 1c5914b1-9eaf-3aa7-a0d9-cf11c0a72e10",
+  //         // 'Content-Type': 'application/x-www-form-urlencoded',
+  //       },
+  //       redirect: "follow", // manual, *follow, error
+  //       referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+  //       body: JSON.stringify(data), // body data type must match "Content-Type" header
+  //     })
+  //       .then((res) => res.json())
+  //       .then((data: string | any) => {
+  //         console.log(data);
+  //         setData(data);
+  //         setLoading(false);
+  //       });
+  //   }
+
+  //   getData("https://eu.ipaas.samsung.com/eu/gcic/GetSOPartsInfo/1.0/ImportSet", {
+  //     IvSvcOrderNo: "4266443508",
+  //     IvAscJobNo: "4266443508",
+  //     IsCommonHeader: {
+  //       Company: "C720",
+  //       AscCode: "1730640",
+  //       Lang: "EN",
+  //       Country: "ZA",
+  //       Pac: "999999920180502152320",
+  //     },
+  //   });
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [search]);
+
+
+  if (isLoading) return <p>Loading...</p>;
   const handleSubmit = (event: any) => {
     event.preventDefault();
     setSearch(event.target.value);
@@ -19,12 +63,12 @@ export default function Management() {
           <section className="my-4 flex justify-center flex-col items-center gap-2">
             <form onSubmit={handleSubmit} className="flex flex-row gap-2">
               <label htmlFor="searchPart" className="sr-only">
-                Search Part
+                Search here
               </label>
               <input
                 className="searchInput search input placeholder-sky-900 outline-none text-gray-950"
-                placeholder="Search Part"
-                type="text"
+                placeholder="Search Service Order No."
+                type="search"
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
               />
@@ -35,6 +79,8 @@ export default function Management() {
               />
             </form>
           </section>
+
+          <section></section>
         </div>
       </main>
     </>
