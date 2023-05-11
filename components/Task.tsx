3d440ba48acee6df855ca06b9176c8ace96425aa
Katleho <katlehomabala3@gmail.com>
2023-05-11 11:21:36 +0200

    style - added some styling to task card

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import { TaskModel } from '../utils/models'
+import React from "react";
+import { TaskModel } from "../utils/models";
+import { TrashIcon } from "@heroicons/react/24/outline";
 
 type TaskProps = {
-index:number;
-task:TaskModel;
-}
+  index: number;
+  task: TaskModel;
+};
 
-function Task({index, task}: TaskProps) {
+function Task({ index, task }: TaskProps) {
   return (
-    <article className='relative rounded w-200 pl-3 pr-7 pt-3 pb-1 shadow-sm cursor-grab'>
-
-
+    <article className="relative rounded w-200 pl-3 pr-7 pt-3 pb-1 shadow-sm cursor-grab">
+      <TrashIcon className="absolute top-0 right-0 bg-gray-700 opacity-0 hover:opacity-10" />
+      <label htmlFor="w3review" className="sr-only">
+        {""}
+      </label>
+      <textarea id="w3review" name="w3review" value={task.title} className="font-semibold cursor-inherit border-0 resize-none min-h-70 max-h-200 focus:outline-none focus:border-0"></textarea>
     </article>
-  )
+  );
 }
 
-export default Task 
+export default Task;
