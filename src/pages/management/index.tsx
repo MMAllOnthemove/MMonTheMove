a09ee6865eb09e89ae89146f730a93b877d40566
Katleho <katlehomabala3@gmail.com>
2023-05-11 10:33:03 +0200

    fix - removed the kanban component had errors

--- src/pages/management/index.tsx
+++ src/pages/management/index.tsx
@@ -1,113 +1,6 @@
 import { memo, useState, useEffect } from "react";
 import Navbar from "../../../components/Navbar";
-import { Formik } from "formik";
-import Board, {moveCard} from "@asseinfo/react-kanban";
-import '@asseinfo/react-kanban/dist/styles.css'
 
-
-const board = {
-  columns: [
-    {
-      id: 1,
-      title: "Backlog",
-      backgroundColor: "#fff",
-      cards: [
-        {
-          id: 1,
-          title: "Card title 1",
-          description: "Card content"
-        },
-        {
-          id: 2,
-          title: "Card title 2",
-          description: "Card content"
-        },
-        {
-          id: 3,
-          title: "Card title 3",
-          description: "Card content"
-        }
-      ]
-    },
-    {
-      id: 2,
-      title: "Doing",
-      cards: [
-        {
-          id: 9,
-          title: "Card title 9",
-          description: "Card content"
-        }
-      ]
-    },
-    {
-      id: 3,
-      title: "Q&A",
-      cards: [
-        {
-          id: 10,
-          title: "Card title 10",
-          description: "Card content"
-        },
-        {
-          id: 11,
-          title: "Card title 11",
-          description: "Card content"
-        }
-      ]
-    },
-    {
-      id: 4,
-      title: "Production",
-      cards: [
-        {
-          id: 12,
-          title: "Card title 12",
-          description: "Card content"
-        },
-        {
-          id: 13,
-          title: "Card title 13",
-          description: "Card content"
-        }
-      ]
-    }
-  ]
-};
-function ControlledBoard() {
-  // You need to control the state yourself.
-  const [controlledBoard, setBoard] = useState(board);
-
-  function handleCardMove(_card, source, destination) {
-    const updatedBoard = moveCard(controlledBoard, source, destination);
-    setBoard(updatedBoard);
-  }
-
-  return (
-    <Board onCardDragEnd={handleCardMove} disableColumnDrag>
-      {controlledBoard}
-    </Board>
-  );
-}
-function UncontrolledBoard() {
-  return (
-    <Board
-      allowRemoveLane
-      allowRenameColumn
-      allowRemoveCard
-      onLaneRemove={console.log}
-      onCardRemove={console.log}
-      onLaneRename={console.log}
-      initialBoard={board}
-      allowAddCard={{ on: "top" }}
-      onNewCardConfirm={(draftCard) => ({
-        id: new Date().getTime(),
-        ...draftCard
-      })}
-      onCardNew={console.log}
-    />
-  );
-}
 
 const Management = () => {
   const [data, setData] = useState<null | any>(null);
@@ -340,7 +233,6 @@ const Management = () => {
             </table>
           </section> */}
           <section>
-<ControlledBoard/>
           </section>
         </div>
       </main>
