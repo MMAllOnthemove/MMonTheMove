d4a09e320388b5e723d6d630bca226c83156a98c
Katleho <katlehomabala3@gmail.com>
2023-05-11 11:06:22 +0200

    created article card in Task component

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { TaskModel } from '../utils/models'
 
-function Task() {
+type TaskProps = {
+index:number;
+task:TaskModel;
+}
+
+function Task({index, task}: TaskProps) {
   return (
-    <div>Task</div>
+    <article className='relative rounded w-200 pl-3 pr-7 pt-3 pb-1 shadow-sm cursor-grab'>
+
+
+    </article>
   )
 }
 
