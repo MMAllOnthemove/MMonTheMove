b48424bdf9a3e79c9f816e67f56f508fc07dfd15
Katleho <katlehomabala3@gmail.com>
2023-05-12 09:46:09 +0200

    feat - created kanban board ui on & refactored

---
+++ components/NewItemButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import { NewItemButton } from '@/styles/style'
+
+const NewItemButton = () => {
+  return (
+    <div>NewItemButton</div>
+  )
+}
+
+export default NewItemButton
