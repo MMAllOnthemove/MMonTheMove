5047475ff7f3a4b88fe667e47ae5ab0fa24b10db
Katleho <katlehomabala3@gmail.com>
2023-05-12 16:55:56 +0200

    REFACTORED

--- components/Card.tsx
+++
@@ -1,8 +0,0 @@
-import { CardContainer } from "@/styles/style";
-
-type CardProps = {
-  text: string;
-};
-export const Card = ({ text }: CardProps) => {
-  return <CardContainer>{text}</CardContainer>;
-};
