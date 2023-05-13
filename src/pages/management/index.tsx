1dcdc088abc611405b78ea8a50bb7d6aef0123f8
Katleho <katlehomabala3@gmail.com>
2023-05-13 09:44:12 +0200

    Feat - added headles ui tabs in management

--- src/pages/management/index.tsx
+++ src/pages/management/index.tsx
@@ -1,5 +1,12 @@
 import { memo, useEffect, useState } from "react";
 import Navbar from "../../../components/Navbar";
+import { Tab } from "@headlessui/react";
+
+// Classes
+
+function classNames(...classes: string[] | any[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Management = () => {
   const [data, setData] = useState<null | any>(null);
@@ -231,7 +238,79 @@ const Management = () => {
               </tbody>
             </table>
           </section> */}
-          <section></section>
+          <section>
+            <div className="tabs w-full max-w-full px-2 py-16 sm:px-0">
+              <Tab.Group>
+                <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+                  <Tab
+                    className={({ selected }) =>
+                      classNames(
+                        "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                        "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                        selected
+                          ? "bg-white shadow"
+                          : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                      )
+                    }
+                  >
+                    Tab 1
+                  </Tab>
+                  <Tab
+                    className={({ selected }) =>
+                      classNames(
+                        "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                        "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                        selected
+                          ? "bg-white shadow"
+                          : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                      )
+                    }
+                  >
+                    Tab 2
+                  </Tab>
+                  <Tab
+                    className={({ selected }) =>
+                      classNames(
+                        "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700",
+                        "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                        selected
+                          ? "bg-white shadow"
+                          : "text-blue-100 hover:bg-white/[0.12] hover:text-white"
+                      )
+                    }
+                  >
+                    Tab 3
+                  </Tab>
+                </Tab.List>
+                <Tab.Panels className="mt-2">
+                  <Tab.Panel
+                    className={classNames(
+                      "rounded-xl bg-white p-3",
+                      "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
+                    )}
+                  >
+                    Content 1
+                  </Tab.Panel>
+                  <Tab.Panel
+                    className={classNames(
+                      "rounded-xl bg-white p-3",
+                      "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
+                    )}
+                  >
+                    Content 2
+                  </Tab.Panel>
+                  <Tab.Panel
+                    className={classNames(
+                      "rounded-xl bg-white p-3",
+                      "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
+                    )}
+                  >
+                    Content 3
+                  </Tab.Panel>
+                </Tab.Panels>
+              </Tab.Group>
+            </div>
+          </section>
         </div>
       </main>
     </>
