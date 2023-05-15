582fc34dedbbc1dbbc4e25866a0eb1d1689265da
Katleho <katlehomabala3@gmail.com>
2023-05-15 11:29:22 +0200

    feat - added kanban board

--- components/ManagementTabOneHeader.tsx
+++ components/ManagementTabOneHeader.tsx
@@ -2,10 +2,11 @@ import AddEditBoardModal from "@/modals/AddEditBoardModal";
 import AddEditTaskModal from "@/modals/AddEditTaskModal";
 import DeleteModal from "@/modals/DeleteModal";
 import boardsSlice from "@/redux/boardsSlice";
-import { useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import ElipsisMenu from "./ElipsisMenu";
+// import ElipsisMenu from "./ElipsisMenu";
 import HeaderDropDown from "./ManagementTabOneHeaderDropdown";
+import { Menu, Transition } from "@headlessui/react";
 
 function ManagementTabOneHeader({ setIsBoardModalOpen, isBoardModalOpen }) {
   const [openDropdown, setOpenDropdown] = useState(false);
@@ -55,7 +56,7 @@ function ManagementTabOneHeader({ setIsBoardModalOpen, isBoardModalOpen }) {
         {/* Right Side */}
 
         <div className="flex place-content-end items-center gap-2">
-          <button
+          {/* <button
             className="border rounded"
             onClick={() => {
               setIsBoardModalOpen(true);
@@ -78,11 +79,119 @@ function ManagementTabOneHeader({ setIsBoardModalOpen, isBoardModalOpen }) {
             className=" button py-1 px-3 md:hidden "
           >
             +
-          </button>
+          </button> */}
+
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                Menu
+                {/* <ChevronDownIcon
+                    className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
+                    aria-hidden="true"
+                  /> */}
+              </Menu.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 mt-2 w-100 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="px-1 py-1 ">
+                  <Menu.Item>
+                    <button
+                      className={`
+                              text-gray-900
+                          group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Edit
+                    </button>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <button
+                      className={`
+                              text-gray-900
+                          group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Duplicate
+                    </button>
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+
+          <Menu as="div" className="relative inline-block text-left">
+            <div
+              onClick={() => {
+                setBoardType("edit");
+                setOpenDropdown(false);
+                setIsElipsisMenuOpen((prevState) => !prevState);
+              }}
+            >
+              <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+                  />
+                </svg>
+              </Menu.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 mt-2 w-100 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="px-1 py-1 ">
+                  <Menu.Item>
+                    <button
+                      onClick={() => {
+                        setOpenEditModal();
+                      }}
+                      className={`
+                              text-gray-900
+                          group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Edit
+                    </button>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <button
+                      onClick={() => setOpenDeleteModal()}
+                      className={`
+                              text-gray-900
+                          group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Delete
+                    </button>
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+          {/* 
           <svg
             onClick={() => {
-              setBoardType("edit");
-              setOpenDropdown(false);
+              // setBoardType("edit");
+              // setOpenDropdown(false);
               setIsElipsisMenuOpen((prevState) => !prevState);
             }}
             xmlns="http://www.w3.org/2000/svg"
@@ -97,7 +206,7 @@ function ManagementTabOneHeader({ setIsBoardModalOpen, isBoardModalOpen }) {
               strokeLinejoin="round"
               d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
             />
-          </svg>
+          </svg> */}
 
           {/* <img
             onClick={() => {
@@ -110,11 +219,44 @@ function ManagementTabOneHeader({ setIsBoardModalOpen, isBoardModalOpen }) {
             className=" cursor-pointer"
           /> */}
           {isElipsisMenuOpen && (
-            <ElipsisMenu
-              type="Boards"
-              setOpenEditModal={setOpenEditModal}
-              setOpenDeleteModal={setOpenDeleteModal}
-            />
+            <Menu>
+              <Menu.Items
+                type="Boards"
+                setOpenEditModal={setOpenEditModal}
+                setOpenDeleteModal={setOpenDeleteModal}
+                className="absolute right-0 mt-2 w-100 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              >
+                <div className="px-1 py-1 ">
+                  <Menu.Item>
+                    <button
+                      onClick={() => {
+                        setOpenEditModal();
+                      }}
+                      className={`
+                            text-gray-900
+                        group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Edit
+                    </button>
+                  </Menu.Item>
+                  <Menu.Item>
+                    <button
+                      onClick={() => setOpenDeleteModal()}
+                      className={`
+                            text-gray-900
+                        group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Delete
+                    </button>
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Menu>
+            // <ElipsisMenu
+            //   type="Boards"
+            //   setOpenEditModal={setOpenEditModal}
+            //   setOpenDeleteModal={setOpenDeleteModal}
+            // />
           )}
         </div>
 
