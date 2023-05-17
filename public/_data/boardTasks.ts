bae2b246f50715c37e96b696807226ee33e7e8be
Katleho <katlehomabala3@gmail.com>
2023-05-17 10:01:41 +0200

    added new data file for dummy tasks & boards

---
+++ public/_data/boardTasks.ts
@@ -0,0 +1,16 @@
+const boards = [
+    {
+        name: "", isActive: false,
+        columns: [
+            {
+                name: "Todo",
+                id: 1,
+                tasks: [
+
+                ]
+            }
+        ]
+
+
+    }
+]
