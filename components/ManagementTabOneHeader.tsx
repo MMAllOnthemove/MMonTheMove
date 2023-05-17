9adcd9e22ed92cd7523689687d758a801cbb44ec
Katleho <katlehomabala3@gmail.com>
2023-05-17 10:01:41 +0200

    added new data file for dummy tasks & boards

--- components/ManagementTabOneHeader.tsx
+++ components/ManagementTabOneHeader.tsx
@@ -58,7 +58,7 @@ function ManagementTabOneHeader({ setIsBoardModalOpen, isBoardModalOpen }) {
           <Menu as="div" className="relative inline-block text-left">
             <div>
               <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                Menu
+                Add
                 {/* <ChevronDownIcon
                     className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
                     aria-hidden="true"
@@ -74,24 +74,30 @@ function ManagementTabOneHeader({ setIsBoardModalOpen, isBoardModalOpen }) {
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             >
-              <Menu.Items className="absolute right-0 mt-2 w-100 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <Menu.Items className="absolute right-0 mt-2 w-auto origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                 <div className="px-1 py-1 ">
                   <Menu.Item>
                     <button
+                      onClick={() => {
+                        setIsBoardModalOpen(true);
+                      }}
                       className={`
                               text-gray-900
                           group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                     >
-                      Edit
+                      Column
                     </button>
                   </Menu.Item>
                   <Menu.Item>
                     <button
+                      onClick={() => {
+                        setIsTaskModalOpen((prevState) => !prevState);
+                      }}
                       className={`
                               text-gray-900
                           group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                     >
-                      Duplicate
+                      Task
                     </button>
                   </Menu.Item>
                 </div>
@@ -108,20 +114,11 @@ function ManagementTabOneHeader({ setIsBoardModalOpen, isBoardModalOpen }) {
               }}
             >
               <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
-                  />
-                </svg>
+                Options
+                {/* <ChevronDownIcon
+                    className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
+                    aria-hidden="true"
+                  /> */}
               </Menu.Button>
             </div>
             <Transition
