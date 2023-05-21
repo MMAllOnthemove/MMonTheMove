2eec8b66d07bd2bb86b0fb1396c9860bfac97f31
Katleho Janco <katlehomabala3@gmail.com>
2023-05-21 15:48:15 +0200

    fix - commented out the actions.ts file

--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,51 +1,49 @@
+// export const addTask = (
+//     text: string,
+//     listId: string,
+// ): Action => ({
+//     type: "ADD_TASK",
+//     payload: {
+//         text,
+//         listId
+//     }
+// })
+// export const addList = (
+//     text: string,
+// ): Action => ({
+//     type: "ADD_LIST",
+//     payload: text
+// })
 
+// export const moveList = (
+//     draggedId: string,
+//     hoverId: string,
+// ): Action => ({
+//     type: "MOVE_LIST",
+//     payload: {
+//         draggedId,
+//         hoverId,
+//     }
+// })
 
-export const addTask = (
-    text: string,
-    listId: string,
-): Action => ({
-    type: "ADD_TASK",
-    payload: {
-        text,
-        listId
-    }
-})
-export const addList = (
-    text: string,
-): Action => ({
-    type: "ADD_LIST",
-    payload: text
-})
+// export const setDraggedItem = (
+//     draggedItem: DragItem | null,
+// ): Action => ({
+//     type: "SET_DRAGGED_ITEM",
+//     payload: draggedItem
+// })
 
-export const moveList = (
-    draggedId: string,
-    hoverId: string,
-): Action => ({
-    type: "MOVE_LIST",
-    payload: {
-        draggedId,
-        hoverId,
-    }
-})
-
-export const setDraggedItem = (
-    draggedItem: DragItem | null,
-): Action => ({
-    type: "SET_DRAGGED_ITEM",
-    payload: draggedItem
-})
-
-export const moveTask = (
-    draggedItemId: string,
-    hoveredItemId: string | null,
-    sourceColumnId: string,
-    targetColumnId: string
-): Action => ({
-    type: "MOVE_TASK",
-    payload: {
-        draggedItemId,
-        hoveredItemId,
-        sourceColumnId,
-        targetColumnId
-    }
-})
+// export const moveTask = (
+//     draggedItemId: string,
+//     hoveredItemId: string | null,
+//     sourceColumnId: string,
+//     targetColumnId: string
+// ): Action => ({
+//     type: "MOVE_TASK",
+//     payload: {
+//         draggedItemId,
+//         hoveredItemId,
+//         sourceColumnId,
+//         targetColumnId
+//     }
+// })
