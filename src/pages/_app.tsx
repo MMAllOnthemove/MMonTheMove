db8b15149e6697747f072f6ba72a8410a17b72ac
Katleho <katlehomabala3@gmail.com>
2023-05-24 12:00:33 +0200

    extended the chakra/ui theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,20 @@ import type { AppProps } from "next/app";
 import { DndProvider } from "react-dnd";
 import { AppStateProvider } from "@/state/AppStateContext";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { extendTheme } from "@chakra-ui/react";
+
+// Extending the chakra/ui theme
+const theme = extendTheme({
+  fonts: {
+    heading: `'Inter Tight Variable', sans-serif`,
+    body: `'Inter Tight Variable', sans-serif`,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppStateProvider>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <DndProvider backend={HTML5Backend}>
           <Component {...pageProps} />
         </DndProvider>
