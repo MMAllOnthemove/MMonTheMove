ddfccd8be6bb222b46f2604792fa03c1201bd6b4
Katleho <katlehomabala3@gmail.com>
2023-05-25 15:35:04 +0200

    style - added some styling to login/signup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
+import { AppStateProvider } from "@/state/AppStateContext";
 import "@/styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { DndProvider } from "react-dnd";
-import { AppStateProvider } from "@/state/AppStateContext";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { extendTheme } from "@chakra-ui/react";
 
 // Extending the chakra/ui theme
 const theme = extendTheme({
