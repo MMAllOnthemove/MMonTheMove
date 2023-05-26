3295dfc6d880298498e118614411ae21e636fac2
Katleho <katlehomabala3@gmail.com>
2023-05-26 12:30:53 +0200

    feat - added an express server

---
+++ server/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors')
+const app = express()
+const pool = require('./db');
+const { v4: uuidv4 } = require('uuid');
+
+
+
+app.use(cors())
+app.use(express.json())
+
+// GET info from database
+app.get("/management", async (req, res)=>{
+	try{
+		const dbData = await pool.query('SELECT * FROM service_orders')
+		res.json(dbData.rows)
+	} catch(err){
+		console.log(err)
+	}
+})
+
+// POST info to database
+app.post("/management", async (req, res)=>{
+	const {service_order, warranty, model, fault, imei, serial_number, engineer,parts_issued, parts_ordered} = req.body;
+try {
+    const results = await pool.query(
+      "INSERT INTO service_orders (service_order, warranty, model, fault, imei, serial_number, engineer, parts_issued, parts_ordered) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning *",
+      [service_order, warranty, model, fault, imei, serial_number, engineer, parts_issued, parts_ordered]
+    );
+    res.status(201).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+
+const PORT = 3001;
+app.listen(PORT, ()=> {
+	console.log(`Server is up and listening on port localhost:${PORT}`);
+})
+module.exports = pool;
