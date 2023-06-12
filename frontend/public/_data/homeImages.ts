be94ccf8556d36c9e9e159d6dd090d22177df3f8
Katleho <katlehomabala3@gmail.com>
2023-06-12 10:23:30 +0200

    style - styled up the home screen

---
+++ frontend/public/_data/homeImages.ts
@@ -0,0 +1,24 @@
+import phone1 from "../images/phone(1).jpg";
+import phone2 from "../images/phone(2).jpg";
+import phone3 from "../images/phone(3).jpg";
+import phone4 from "../images/phone(4).jpg";
+
+
+export const homeImages = [
+    {
+        id: 1,
+        src: phone1
+    },
+    {
+        id: 2,
+        src: phone2
+    },
+    {
+        id: 3,
+        src: phone3
+    },
+    {
+        id: 4,
+        src: phone4
+    }
+]
