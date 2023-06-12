aa4f17e606e95fc74c9585b78d637f4a3626f99f
Katleho <katlehomabala3@gmail.com>
2023-06-12 09:51:27 +0200

    added 4 images of smartphones

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import useDebounce from "../../components/useDebounce";
 import Modal from "../../components/Modals/modal.home";
