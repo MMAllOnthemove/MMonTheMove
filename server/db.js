89d0402d71cdd435bb50cfccecbf4a459b83de9f
Katleho Janco <Katlehomabala3@gmail.com>
2023-06-18 09:17:20 +0200

    changed  a few configs to match .env

--- server/db.js
+++ server/db.js
@@ -1,12 +1,11 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 require("dotenv").config();
-
 
 const pool = new Pool({
   user: process.env.NEXT_PUBLIC_USER,
   host: process.env.NEXT_PUBLIC_HOST,
   database: process.env.NEXT_PUBLIC_DB_NAME,
-  password: process.env.NEXT_PUBLIC_PASSWORD,
+  // password: process.env.NEXT_PUBLIC_PASSWORD,
   port: process.env.NEXT_PUBLIC_PORT,
 });
 
