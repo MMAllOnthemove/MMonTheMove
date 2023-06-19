4ba4c3ec59a795c0d3538a2a6799e30ea2d4831d
Katleho <katlehomabala3@gmail.com>
2023-06-19 11:07:00 +0200

    feat - engineer can now set his/her own analysis

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: process.env.NEXT_PUBLIC_USER,
   host: process.env.NEXT_PUBLIC_HOST,
   database: process.env.NEXT_PUBLIC_DB_NAME,
-  // password: process.env.NEXT_PUBLIC_PASSWORD,
+  password: process.env.NEXT_PUBLIC_PASSWORD,
   port: process.env.NEXT_PUBLIC_PORT,
 });
 
