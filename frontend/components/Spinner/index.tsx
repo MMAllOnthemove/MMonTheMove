a45187ed573c204a18c8c8fecf6aaff47f518635
Katleho <katlehomabala3@gmail.com>
2023-08-10 10:29:20 +0200

    installed cypress, fixed getstaticprops bug

--- frontend/components/Spinner/index.tsx
+++ frontend/components/Spinner/index.tsx
@@ -3,9 +3,9 @@ import { Spinner as Loading } from "@chakra-ui/react";
 
 function Spinner() {
   return (
-    <main className="flex flex-col justify-center h-screen max-h-screen">
+    <main className="flex flex-col justify-center items-center h-screen max-h-screen">
       <Loading size="xl" />
-      <p className="font-sans text-slate-900 font-semobild">
+      <p className="font-sans text-slate-900 font-semibold">
         HHP Management loading...
       </p>
     </main>
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   productionBrowserSourceMaps: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 module.exports = nextConfig;
