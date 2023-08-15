34415b83494ca1ccc107c7ba3909195133b64f78
Katleho <katlehomabala3@gmail.com>
2023-08-15 14:21:49 +0200

    feat - can now add jobs using repairshpr!

---
+++ frontend/src/functions/repair_api.tsx
@@ -0,0 +1,20 @@
+interface IRepairTicketInfo {
+  searchTicketNumber: string;
+  setTicketNumber: (order: string) => void;
+}
+
+export async function getRepairTicketInfo(props: IRepairTicketInfo) {
+  await fetch(`${process.env.NEXT_PUBLIC_REPAIRSHOPR_LINK}`, {
+    method: "GET",
+    mode: "cors",
+    cache: "no-cache",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_REPAIRSHOPR_LINK}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log("data"))
+    .catch((e) => console.log("repair errors", e));
+}
