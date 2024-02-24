3aca9a66ec3361bd41f75c513fa2e8f3acc6e39b
Katleho <katlehomabala3@gmail.com>
2024-02-24 12:35:04 +0200

    fix - updated engineers list

--- frontend/src/atoms/create_checklist_modal_atom.ts
+++ frontend/src/atoms/create_checklist_modal_atom.ts
@@ -1,14 +1,14 @@
 import { atom } from "recoil";
 
 export interface CreateChecklistModalState {
-    open: boolean;
-    view: "dtv-add-checklist";
+  open: boolean;
+  view: "dtv-add-checklist";
 }
 const defaultModalState: CreateChecklistModalState = {
-    open: false,
-    view: 'dtv-add-checklist'
-}
+  open: false,
+  view: "dtv-add-checklist",
+};
 export const createChecklistModalState = atom<CreateChecklistModalState>({
-    key: 'createChecklistModalState',
-    default: defaultModalState
-})
+  key: "createChecklistModalState",
+  default: defaultModalState,
+});
