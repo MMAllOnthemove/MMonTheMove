6fbdbdbbef7cbe261bf7c0b9b60144cf36f7af3a
Katleho Mabala <katleho_m@allelectronics.co.za>
2024-04-29 08:57:10 +0200

    can create tooka tasks by pulling SO info from api instead of typing

---
+++ frontend/utils/gspn_fridge_symptoms.ts
@@ -0,0 +1,1057 @@
+const fridgeSymptom1 = [
+  {
+    id: 1,
+    _name: "M1 - [Installation]",
+    value: "M1",
+  },
+  {
+    id: 2,
+    _name: "M2 - [Design]",
+    value: "M2",
+  },
+  {
+    id: 3,
+    _name: "M3 - [Operation]",
+    value: "M3",
+  },
+  {
+    id: 4,
+    _name: "M4 - [Display/error code]",
+    value: "M4",
+  },
+  {
+    id: 5,
+    _name: "M5 - [Noise/vibrations]",
+    value: "M5",
+  },
+  {
+    id: 6,
+    _name: "M6 - [Ice/water/sparkling]",
+    value: "M6",
+  },
+  {
+    id: 7,
+    _name: "M7 - [Cooling/temperature/condensation issue]",
+    value: "M7",
+  },
+  {
+    id: 8,
+    _name: "M8 - [Door/handle]",
+    value: "M8",
+  },
+  {
+    id: 9,
+    _name: "M9 - [Specification]",
+    value: "M9",
+  },
+  {
+    id: 10,
+    _name: "MZ - [SmartThings]",
+    value: "MZ",
+  },
+  {
+    id: 1,
+    _name: "Z1 - [Galaxy Diagnostic]",
+    value: "Z1",
+  },
+];
+const fridgeSymptom2 = [
+  {
+    id: 1,
+    _name: "01 - [Installation]",
+    value: "01",
+  },
+];
+const fridgeSymptom2A = [
+  {
+    id: 1,
+    _name: "01 - [Missing parts]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Interior design/damage/discolor]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Exterior design/damage/discolor]",
+    value: "03",
+  },
+];
+const fridgeSymptom2B = [
+  {
+    id: 1,
+    _name: "01 - [Light issue]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Power issue]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Leakage current (shock from fridge)]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [WiFi issue]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [Smell issue]",
+    value: "05",
+  },
+];
+const fridgeSymptom2C = [
+  {
+    id: 1,
+    _name: "01 - [Appearance defect]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [OF OF or OFF is displayed]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Button function not responding]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Display status (blinking/no display)]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [LCD screen]",
+    value: "05",
+  },
+  {
+    id: 6,
+    _name: "06 - [Pc-Er/Er-PC Error code]",
+    value: "06",
+  },
+  {
+    id: 7,
+    _name: "07 - [Digit error code]",
+    value: "07",
+  },
+  {
+    id: 8,
+    _name: "08 - [LED error message]",
+    value: "08",
+  },
+];
+const fridgeSymptom2D = [
+  {
+    id: 1,
+    _name: "01 - [Door alarm]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Single noise(within 5 sec)]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Repeated noise(over 5 sec)]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [When dispensing ice]",
+    value: "04",
+  },
+];
+const fridgeSymptom2E = [
+  {
+    id: 1,
+    _name: "01 - [Ice/water dispensing]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Ice making/ice bucket stuck issue]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Leak/dripping]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Appearance defect]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [Sparkling issue]",
+    value: "05",
+  },
+];
+const fridgeSymptom2F = [
+  {
+    id: 1,
+    _name: "01 - [Frost/condensation(moisture)]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Over cooling]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [No cooling]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Weak cooling]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [Exterior temperature-hot]",
+    value: "05",
+  },
+];
+const fridgeSymptom2G = [
+  {
+    id: 1,
+    _name: "01 - [Level/alignment issue]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Operation/noise issue]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Appearance defect]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Handle/damage]",
+    value: "04",
+  },
+];
+const fridgeSymptom2H = [
+  {
+    id: 1,
+    _name: "01 - [Product]",
+    value: "01",
+  },
+];
+const fridgeSymptom2I = [
+  {
+    id: 16,
+    _name: "16 - [Account issue]",
+    value: "16",
+  },
+  {
+    id: 17,
+    _name: "17 - [Connection error]",
+    value: "17",
+  },
+  {
+    id: 18,
+    _name: "18 - [Control error]",
+    value: "18",
+  },
+  {
+    id: 19,
+    _name: "19 - [App function error]",
+    value: "19",
+  },
+  {
+    id: 20,
+    _name: "20 - [How to use]",
+    value: "20",
+  },
+  {
+    id: 21,
+    _name: "21 - [Performance issues]",
+    value: "21",
+  },
+];
+const fridgeSymptom2J = [
+  {
+    id: 1,
+    _name: "01 - [Power]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Battery]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Charging]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Sound]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [Display]",
+    value: "05",
+  },
+  {
+    id: 6,
+    _name: "06 - [Touch]",
+    value: "06",
+  },
+  {
+    id: 8,
+    _name: "08 - [Camera]",
+    value: "08",
+  },
+];
+const fridgeSymptom2K = [
+  {
+    id: 1,
+    _name: "01 - [Galaxy diagnostic]",
+    value: "01",
+  },
+];
+const fridgeSymptom3 = [
+  {
+    id: 1,
+    _name: "01 - [Installation]",
+    value: "01",
+  },
+];
+const fridgeSymptom3A = [
+  {
+    id: 1,
+    _name: "01 - [Shelf]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Door bins/guard]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Vegetable & fruit drawers]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Freezer box/drawers]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [Coolpack]",
+    value: "05",
+  },
+  {
+    id: 6,
+    _name: "06 - [Others]",
+    value: "06",
+  },
+];
+const fridgeSymptom3B = [
+  {
+    id: 1,
+    _name: "01 - [Shelf]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Door bins/guard]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Vegetable & fruit drawers]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Freezer box/drawers]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [Coolpack]",
+    value: "05",
+  },
+  {
+    id: 6,
+    _name: "06 - [Others]",
+    value: "06",
+  },
+];
+const fridgeSymptom3C = [
+  {
+    id: 1,
+    _name: "01 - [Cabinet]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Mascot]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Stand base]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Leg cover]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [Leg/roller]",
+    value: "05",
+  },
+  {
+    id: 6,
+    _name: "06 - [Top table(TRIM)]",
+    value: "06",
+  },
+  {
+    id: 7,
+    _name: "07 - [Others]",
+    value: "07",
+  },
+];
+const fridgeSymptom3D = [
+  {
+    id: 1,
+    _name: "01 - [No light(Freezer room)]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [No light(Middle drawer)]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [No light(Fridge room)]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Weak lightness(Light is dim)]",
+    value: "04",
+  },
+];
+const fridgeSymptom3E = [
+  {
+    id: 1,
+    _name: "01 - [No power]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Intermittent power]",
+    value: "02",
+  },
+];
+const fridgeSymptom3F = [
+  {
+    id: 1,
+    _name: "01 - [Leakage current]",
+    value: "01",
+  },
+];
+const fridgeSymptom3G = [
+  {
+    id: 1,
+    _name: "01 - [Intermittent WiFi connection]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [WiFi disconnection(WiFi not connecting at all)]",
+    value: "02",
+  },
+];
+const fridgeSymptom3H = [
+  {
+    id: 1,
+    _name: "01 - [From inside]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Burning smell]",
+    value: "02",
+  },
+];
+const fridgeSymptom3I = [
+  {
+    id: 1,
+    _name: "01 - [Fall of display(display fell out of unit)]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Damaged display]",
+    value: "02",
+  },
+];
+const fridgeSymptom3J = [
+  {
+    id: 1,
+    _name: "01 - [Demo mode]",
+    value: "01",
+  },
+];
+const fridgeSymptom3K = [
+  {
+    id: 1,
+    _name: "01 - [Touch button]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Push button]",
+    value: "02",
+  },
+];
+const fridgeSymptom3L = [
+  {
+    id: 1,
+    _name: "01 - [No display]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Display digits are incomplete]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Display temperature incorrect]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Red filter icon lit]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [Blinking ice off icon]",
+    value: "05",
+  },
+  {
+    id: 6,
+    _name: "06 - [Blinking temperature of fridge compartment]",
+    value: "06",
+  },
+  {
+    id: 7,
+    _name: "07 - [Blinking temperature of freezer compartment]",
+    value: "07",
+  },
+];
+const fridgeSymptom3M = [
+  {
+    id: 1,
+    _name: "01 - [Does not respond]",
+    value: "01",
+  },
+];
+const fridgeSymptom3N = [
+  {
+    id: 1,
+    _name: "[1 E or 1 C (Freezer room sensor error)]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "[2 E or 2 C (Fridge room sensor error)]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "[4 E or 4 C (Freezer defrost sensor error)]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "[5 E or 5 C (Fridge defrost sensor error)]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "[6 E or 6 C (Ambient sensor error)]",
+    value: "05",
+  },
+  {
+    id: 6,
+    _name: "[7 E or 7 C (Pantry or flex room sensor error)]",
+    value: "06",
+  },
+  {
+    id: 7,
+    _name:
+      "[8 E or 8 C (Ice maker-freezer sensor error)(Ice maker sensor Error-FZ)]",
+    value: "07",
+  },
+  {
+    id: 8,
+    _name: "[9 E or 9 C (Cool select room sensor error)]",
+    value: "08",
+  },
+  {
+    id: 9,
+    _name: "[11 E or 11 C (CoolSelect defrost sensor error)]",
+    value: "09",
+  },
+  {
+    id: 10,
+    _name: "[13 E or 13 C (Humidity sensor error)]",
+    value: "10",
+  },
+  {
+    id: 11,
+    _name: "[14 E or 14 C (Ice maker-fridge sensor error)]",
+    value: "11",
+  },
+  {
+    id: 12,
+    _name: "[15 E or 15 C (Ice-room sensor error)]",
+    value: "12",
+  },
+  {
+    id: 13,
+    _name: "[16 E or 16 C (Fridge humidity sensor error)]",
+    value: "13",
+  },
+  {
+    id: 14,
+    _name: "[17 E or 17 C (Sparkling-tank temp' sensor error)]",
+    value: "14",
+  },
+  {
+    id: 15,
+    _name: "[21 E or 21 C (Freezer room fan error)]",
+    value: "15",
+  },
+  {
+    id: 16,
+    _name: "[22 E or 22 C (Fridge room fan error)]",
+    value: "16",
+  },
+  {
+    id: 17,
+    _name: "[23 E or 23 C (Condenser fan error)]",
+    value: "17",
+  },
+  {
+    id: 18,
+    _name: "[24 E or 24 C (Freezer defrosting error)]",
+    value: "18",
+  },
+  {
+    id: 19,
+    _name: "[25 E or 25 C (Fridge defrosting error)]",
+    value: "19",
+  },
+  {
+    id: 20,
+    _name: "[26 E or 26 C (Ice maker-freezer function error)]",
+    value: "20",
+  },
+  {
+    id: 21,
+    _name: "[27 E or 27 C (Pantry or flex-room damper heater error)]",
+    value: "21",
+  },
+  {
+    id: 22,
+    _name: "[28 E or 28 C (Deodorizer fan error)]",
+    value: "22",
+  },
+  {
+    id: 23,
+    _name: "[29 E or 29 C (Cool select defrosting error)]",
+    value: "23",
+  },
+  {
+    id: 24,
+    _name: "[31 E or 31 C (Cool select room fan error)]",
+    value: "24",
+  },
+  {
+    id: 25,
+    _name: "[33 E or 33 C (Freezer-ice maker supply pipe heator error)]",
+    value: "25",
+  },
+  {
+    id: 26,
+    _name: "[34 E or 34 C (Water-tank fridge heater error)]",
+    value: "26",
+  },
+  {
+    id: 27,
+    _name: "[35 E or 35 C (Earlier restoration of freeze defrosting error)]",
+    value: "27",
+  },
+  {
+    id: 28,
+    _name: "[36 E or 36 C (Earlier restoration of fridge defrosting error)]",
+    value: "28",
+  },
+  {
+    id: 29,
+    _name:
+      "[37 E or 37 C (Earlier restoration of cool select defrosting error)]",
+    value: "29",
+  },
+  {
+    id: 30,
+    _name: "[39 E or 39 C (Ice maker-fridge function error)]",
+    value: "30",
+  },
+  {
+    id: 31,
+    _name: "[40 E or 40 C (Ice room fan error)]",
+    value: "31",
+  },
+  {
+    id: 32,
+    _name: "[41 E or 41 C (Panel PBA - Main PBA communication error)]",
+    value: "32",
+  },
+  {
+    id: 33,
+    _name: "[42 E or 42 C (Mainl Micom - Load Micom communication error)]",
+    value: "33",
+  },
+  {
+    id: 34,
+    _name: "[44 E or 44 C (Main - freezer comp. inverter communication error)]",
+    value: "34",
+  },
+  {
+    id: 35,
+    _name: "[45 E or 45 C (Main - freezer comp. inverter communication error)]",
+    value: "35",
+  },
+  {
+    id: 36,
+    _name: "[47 E or 47 C (Main - dispenser panel communication error)]",
+    value: "36",
+  },
+  {
+    id: 37,
+    _name: "[61 E or 61 C (Ice room drain heater open error)]",
+    value: "37",
+  },
+  {
+    id: 38,
+    _name: "[62 E or 62 C (Chef pantry fan error)]",
+    value: "38",
+  },
+  {
+    id: 39,
+    _name: "[65 E or 65 C (Ice room wall heater open error)]",
+    value: "39",
+  },
+  {
+    id: 40,
+    _name: "[66 E or 66 C (Door handle heater open error)]",
+    value: "40",
+  },
+  {
+    id: 41,
+    _name: "[71 E or 71 C (Abnormal high temperature in freezer)]",
+    value: "41",
+  },
+  {
+    id: 42,
+    _name: "[72 E or 72 C (Abnormal high temperature in fridge)]",
+    value: "42",
+  },
+  {
+    id: 43,
+    _name: "[73 E or 73 C (Abnormal high temperature in cool select)]",
+    value: "43",
+  },
+  {
+    id: 44,
+    _name: "[81 E or 81 C (F comp start failure error)]",
+    value: "44",
+  },
+  {
+    id: 45,
+    _name: "[82 E or 82 C (F comp inverter-PBA IPM fault error)]",
+    value: "45",
+  },
+  {
+    id: 46,
+    _name: "[83 E or 83 C (F compressor abnormal current detection error)]",
+    value: "46",
+  },
+  {
+    id: 47,
+    _name: "[84 E or 84 C (F compressor motor locked or over RPM error)]",
+    value: "47",
+  },
+  {
+    id: 48,
+    _name: "[85 E or 85 C (F compressor under voltage error)]",
+    value: "48",
+  },
+  {
+    id: 49,
+    _name: "[86 E or 86 C (F compressor over voltage error)]",
+    value: "49",
+  },
+  {
+    id: 50,
+    _name: "[87 E or 87 C (R comp start failure error)]",
+    value: "50",
+  },
+  {
+    id: 51,
+    _name: "[88 E or 88 C (R comp inverter-PBA IPM fault error)]",
+    value: "51",
+  },
+  {
+    id: 52,
+    _name: "[89 E or 89 C (R compressor abnormal current detection error)]",
+    value: "52",
+  },
+  {
+    id: 53,
+    _name: "[90 E or 90 C (R compressor motor locked or over RPM error)]",
+    value: "53",
+  },
+  {
+    id: 54,
+    _name: "[91 E or 91 C (R compressor under voltage error)]",
+    value: "54",
+  },
+  {
+    id: 55,
+    _name: "[92 E or 92 C (R compressor over voltage error)]",
+    value: "55",
+  },
+  {
+    id: 56,
+    _name: "[93 E or 93 C (Sparkling tank overflow)]",
+    value: "56",
+  },
+  {
+    id: 57,
+    _name: "[94 E or 94 C (Water supply to sparkling tank error)]",
+    value: "57",
+  },
+];
+const fridgeSymptom3O = [
+  {
+    id: 1,
+    _name: "01 - [Right-1 LED On (Ice maker sensor error)]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Right-2 LED On (Panel -main PBA communication error)]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Right-3 LED On (Compressor error)]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Right-4 LED On (Fridge room sensor error)]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [Right-5 LED On (Fridge room damper heater error)]",
+    value: "05",
+  },
+  {
+    id: 6,
+    _name: "06 - [Right-6 LED On]",
+    value: "06",
+  },
+  {
+    id: 7,
+    _name: "07 - [Right-7 LED On (Ice maker function error)]",
+    value: "07",
+  },
+  {
+    id: 8,
+    _name: "08 - [Left-1 LED On (Ambient sensor error)]",
+    value: "08",
+  },
+  {
+    id: 9,
+    _name: "09 - [Left-2 LED On (Freezer room sensor error)]",
+    value: "09",
+  },
+  {
+    id: 10,
+    _name: "10 - [Left-3 LED On (Freezer defrost sensor error)]",
+    value: "10",
+  },
+  {
+    id: 11,
+    _name: "11 - [Left-4 LED On (Freezer room fan error)]",
+    value: "11",
+  },
+  {
+    id: 12,
+    _name: "12 - [Left-5 LED On (Condenser fan error)]",
+    value: "12",
+  },
+  {
+    id: 13,
+    _name: "13 - [Left-6 LED On (Humidity sensor error)]",
+    value: "13",
+  },
+  {
+    id: 14,
+    _name: "14 - [Left-7 LED On (Freezer defrost error)]",
+    value: "14",
+  },
+];
+const fridgeSymptom3P = [
+  {
+    id: 1,
+    _name: "01 - [Door alarm (Door open alarm)]",
+    value: "01",
+  },
+];
+const fridgeSymptom3Q = [
+  {
+    id: 1,
+    _name: "01 - [Popping noise]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Ice dumping noise]",
+    value: "02",
+  },
+];
+const fridgeSymptom3R = [
+  {
+    id: 1,
+    _name: "01 - [Bubbling noise]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Stream noise]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Rattling noise]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Buzzing noise]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [Keep running noise]",
+    value: "05",
+  },
+];
+const fridgeSymptom3S = [
+  {
+    id: 1,
+    _name: "01 - [Grinding noise]",
+    value: "01",
+  },
+];
+const fridgeSymptom3T = [
+  {
+    id: 1,
+    _name: "01 - [Ice type is not selected correctly: crushed, cubed]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [No ice dispensing]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [No water dispensing]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [Ice dispensing with foreign material]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [Weak water stream]",
+    value: "05",
+  },
+  {
+    id: 6,
+    _name: "06 - [Warm water]",
+    value: "06",
+  },
+  {
+    id: 7,
+    _name: "07 - [Taste & odor]",
+    value: "07",
+  },
+];
+const fridgeSymptom3U = [
+  {
+    id: 1,
+    _name: "01 - [Ice room or bucket frost]",
+    value: "01",
+  },
+  {
+    id: 2,
+    _name: "02 - [Low ice production-freezer ice maker]",
+    value: "02",
+  },
+  {
+    id: 3,
+    _name: "03 - [Low ice production-fridge ice maker]",
+    value: "03",
+  },
+  {
+    id: 4,
+    _name: "04 - [No ice production-freezer ice maker]",
+    value: "04",
+  },
+  {
+    id: 5,
+    _name: "05 - [No ice production-fridge ice maker]",
+    value: "05",
+  },
+];
