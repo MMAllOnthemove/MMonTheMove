6527927035d3bea9d6ab23b6c1521cc3018dc54e
Katleho Mabala <katleho_m@allelectronics.co.za>
2024-07-17 11:15:47 +0200

    feat - gspn dashboard works fine

--- frontend/src/components/DashboardCard/index.tsx
+++ frontend/src/components/DashboardCard/index.tsx
@@ -9,7 +9,7 @@ type TDashboardCard = {
 function DashboardCard({ title, paragraph, onClick }: TDashboardCard) {
     return (
 
-        <article className="rounded-lg pt-2 border-[#eee]" onClick={onClick}>
+        <article className="rounded-lg pt-2 border-[#eee] cursor-pointer" onClick={onClick}>
             <div className="rounded-t-none rounded-md p-8 flex justify-center items-center flex-col shadow">
                 <h5 className="mb-2 font-bold tracking-tight text-gray-900">{title}</h5>
                 <p className="font-normal text-gray-700 text-2xl">{paragraph}</p>
--- frontend/src/components/DashboardTables/gspn/Dashboard.tsx
+++ frontend/src/components/DashboardTables/gspn/Dashboard.tsx
@@ -1,26 +1,16 @@
-// External imports
-import {
-    fetchCurrentUser,
-    fetchIPAASSoInfolIST,
-} from "@/hooks/useFetch";
-import dynamic from "next/dynamic";
-
-import { useMemo, useState } from "react";
-
-import moment from "moment";
-
+import Container from "@/components/Container";
 import DashboardCard from "@/components/DashboardCard";
 import PageTitle from "@/components/PageTitle";
-import ToTopButton from "@/components/ToTopButton";
+import { fetchCurrentUser, fetchIPAASSoInfolIST } from "@/hooks/useFetch";
+import { BarChart, LineChart } from "@tremor/react";
+import moment from "moment";
 import Link from "next/link";
+import { useMemo, useState } from "react";
+
+import ToTopButton from "@/components/ToTopButton";
 import Modal from "@/components/PopupModal";
-import Container from "@/components/Container";
-const Pagination = dynamic(() => import("@/components/Table/Pagination"));
-
-
-
-
-
+import NotLoggedIn from "@/components/NotLoggedIn";
+import { gspnDeps } from "../../../../utils/gspn_departments";
 
 
 function Dashboard() {
@@ -28,13 +18,13 @@ function Dashboard() {
     const [dateFrom, setDateFrom] = useState('')
     const [dateTo, setDateTo] = useState('')
     const [department, setDepartment] = useState("")
-
-
-
-
     let momentFormattedDate = moment(dateFrom).format("YYYYMMDD")
 
     const { findAllJobsGSPN } = fetchIPAASSoInfolIST(momentFormattedDate);
+
+    const [isModalOpen, setIsModalOpen] = useState<null | boolean | any>()
+
+
 
     // Get today's date
     const today = new Date();
@@ -44,202 +34,244 @@ function Dashboard() {
     minDate.setMonth(today.getMonth() - 3);
 
     // Format the dates to 'YYYY-MM-DD'
-    const formatDate = (date) => {
+    const formatDate = (date: any) => {
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
         return `${year}-${month}-${day}`;
     };
 
-
-    // const filteredData = useMemo(() => {
-    //     return findAllJobsGSPN?.filter(item => {
-    //         const matchesDepartment = department?.length > 0 && department === item.SvcProduct;
-    //         const matchesDate = momentFormattedDate && item.ReqDate === momentFormattedDate;
-    //         if (department) return matchesDepartment;
-    //         if (matchesDate) return matchesDate;
-    //         if (department?.length > 0 && momentFormattedDate) return matchesDepartment && matchesDate;
-
-    //     });
-    // }, [department, momentFormattedDate, findAllJobsGSPN]);
-
-    const filteredData = findAllJobsGSPN?.filter(item => {
-        // return (!momentFormattedDate || item.ReqDate === momentFormattedDate) && (!department || item.SvcProduct === department);
+    const filteredData = findAllJobsGSPN?.filter((item: any) => {
 
         const itemDate = moment(item.ReqDate)?.format("YYYYMMDD")
 
         const endDate = moment(dateTo).format("YYYYMMDD")
+
         const isWithinDateRange =
             (!momentFormattedDate || itemDate >= momentFormattedDate) &&
             (!endDate || itemDate <= endDate);
         const departmentMatch = !department || item.SvcProduct === department;
+
         return isWithinDateRange && departmentMatch;
     })
-    // useMemo(() => console.log(filteredData?.filter(item => item.WarrantyType === "I" && item.SvcProduct === "HHP")), [momentFormattedDate, department, dateTo])
-    const inWarrantyCount = useMemo(() => filteredData?.filter(item => item.WarrantyType === "I").length, [filteredData]);
-    const outOfWarrantyCount = useMemo(() => filteredData?.filter(item => item.WarrantyType === 'O').length, [filteredData])
-    const pendingCount = useMemo(() => filteredData?.filter(item => item.StatusDesc === "Pending").length, [filteredData])
-    const completedCount = useMemo(() => filteredData?.filter(item => item.StatusDesc === "Completed" || item.StatusDesc === "Complete").length, [filteredData])
-    const goodsDeliveredCount = useMemo(() => filteredData?.filter(item => item.StatusDesc === "Goods Delivered").length, [filteredData])
+
+
+    // Getting the data for when a card is clicked
+    const totalForThatDayDisplay = useMemo(() => {
+        if (filteredData) return filteredData?.filter((item: any) => item.SvcOrderNo);
+        else return []
+    }, [filteredData]);
+    const assignedToServiceCenter = useMemo(() => {
+        if (filteredData) return filteredData?.filter((item: any) => item.StatusDesc === "Assigned to Service Center");
+        else return []
+
+    }, [filteredData])
+    const acknowledgedCount = useMemo(() => {
+        if (filteredData) return filteredData?.filter((item: any) => item.StatusDesc === "Acknowledged by ASC")
+        else return []
+    }, [filteredData])
+    const pendingCount = useMemo(() => {
+        if (filteredData) return filteredData?.filter((item: any) => item.StatusDesc === "Pending");
+        else return []
+    }, [filteredData])
+    const completedCount = useMemo(() => {
+        if (filteredData) return filteredData?.filter((item: any) => item.StatusDesc === "Repair Completed");
+        else return []
+    }, [filteredData])
+    const cancelledCount = useMemo(() => {
+        if (filteredData) return filteredData?.filter((item: any) => item.StatusDesc === "Cancelled by ASC");
+        else return []
+    }, [filteredData])
+    const engineerAssignedCount = useMemo(() => {
+        if (filteredData) return filteredData?.filter((item: any) => item.StatusDesc === "Engineer Assigned");
+        else return []
+    }, [filteredData])
+    const goodsDeliveredCount = useMemo(() => {
+
+        if (filteredData) return filteredData?.filter((item: any) => item.StatusDesc === "Goods Delivered")
+        else return []
+    }, [filteredData])
+
+
+    // Using the data above and creating an array of objects for the card info
+    const displayCards = [{
+        id: 1,
+        title: "Total Jobs",
+        value: totalForThatDayDisplay.length,
+        details: totalForThatDayDisplay
+
+    }, {
+        id: 2,
+        title: "Assigned to Service Center",
+        value: assignedToServiceCenter.length,
+        details: assignedToServiceCenter
+
+    }, {
+        id: 3,
+        title: "Acknowledged by ASC",
+        value: acknowledgedCount.length,
+        details: acknowledgedCount
+
+    }, {
+        id: 4,
+        title: "Pending",
+        value: pendingCount.length,
+        details: pendingCount
+
+    }, {
+        id: 5,
+        title: "Repair Completed",
+        value: completedCount.length,
+        details: completedCount
+
+    }, {
+        id: 6,
+        title: "Cancelled by ASC",
+        value: cancelledCount.length,
+        details: cancelledCount
+
+    }, {
+        id: 7,
+        title: "Engineer Assigned",
+        value: engineerAssignedCount.length,
+        details: engineerAssignedCount
+
+    }, {
+        id: 8,
+        title: "Goods Delivered",
+        value: goodsDeliveredCount.length,
+        details: goodsDeliveredCount
+
+    }];
 
 
 
-
-    const items = [
-        { title: 'In warranty', count: 10 },
-        { title: 'Out of warranty', count: 20 },
-        { title: 'Pending', count: 30 },
-        { title: 'Completed', count: 40 },
-        { title: 'Goods delivered', count: 50 },
-    ];
-
-
-
-    const [showModal, setShowModal] = useState<boolean | null | any>()
-    const [selectedCard, setSelectedCard] = useState(null);
-    const openModal = (card) => {
-        setSelectedCard(card);
-        setShowModal(true);
+    const handleCardClick = (row: any) => {
+        setIsModalOpen(row);
     };
 
     const closeModal = () => {
-        setShowModal(false);
+        setIsModalOpen(false);
     };
 
 
-    // console.log(compressedString);  // Output: "a2b1c5a3"
 
     return (
         <Container>
 
             <PageTitle title="Dashboard" hasSpan={true} spanText={"GSPN"} />
-            <p className="text-gray-600 text-sm text-center my-3 dark:text-[#eee]">
-                The stats here are only gspn based, for {" "}
-                <Link
-                    href="/dashboard/rs"
-                    className="text-blue-600 hover:text-blue-500 font-semibold"
-                >
-                    Repairshopr
-                </Link>
-            </p>
-            <div className="flex justify-between items-center">
-                <span>
-                    <label htmlFor="dateFrom" className="sr-only">
-                        Date from
-                    </label>
-                    <input
-                        type="date"
-                        name="dateFrom"
-                        min={formatDate(minDate)}
-                        max={formatDate(today)}
-                        value={dateFrom}
-                        onChange={(e) => setDateFrom(e.target.value)}
-                        className="mb-2 cursor-pointer bg-white dark:bg-[#22303C] dark:text-[#eee] dark:accent-[#eee] border border-gray-300 outline-0 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block p-2.5"
-                        id="dateFrom"
-                    />
-                </span>
-                <span>
-                    <label htmlFor="dateTo" className="sr-only">
-                        Date To
-                    </label>
-                    <input
-                        type="date"
-                        name="dateTo"
-                        min={formatDate(minDate)}
-                        max={formatDate(today)}
-                        value={dateTo}
-                        onChange={(e) => setDateTo(e.target.value)}
-                        className="mb-2 cursor-pointer bg-white dark:bg-[#22303C] dark:text-[#eee] dark:accent-[#eee] border border-gray-300 outline-0 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block p-2.5"
-                        id="dateTo"
-                    />
-                </span>
-                <span>
-                    <label
-                        htmlFor="engineerFilter"
-                        className="block mb-2 text-sm font-medium  text-gray-900 dark:text-[#eee] sr-only"
-                    >
-                        department filter
-                    </label>
-                    <select
-                        value={department}
-                        onChange={(e) => setDepartment(e.target.value)}
-                        id="department"
-                        className="mb-2 bg-white border cursor-pointer border-gray-300 outline-0 dark:bg-[#22303C] dark:text-[#eee] text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                    >
-                        <option disabled value="">
-                            Filter by department
-                        </option>
+            {!userData ? (
+                <NotLoggedIn />
+            ) : (
+                <>
+                    {/* <p className="text-gray-600 text-sm text-center my-3 dark:text-[#eee]">
+                        The stats here are only gspn based, for {" "}
+                        <Link
+                            href="/dashboard/rs"
+                            className="text-blue-600 hover:text-blue-500 font-semibold"
+                        >
+                            Repairshopr
+                        </Link>
+                    </p> */}
+                    {/* Date range and departments filter */}
+                    <div className="flex justify-between items-center">
+                        <div className="flex gap-2 items-center">
+                            <span>
+                                <label htmlFor="dateFrom" className="sr-only">
+                                    Date from
+                                </label>
+                                <input
+                                    type="date"
+                                    name="dateFrom"
+                                    min={formatDate(minDate)}
+                                    max={formatDate(today)}
+                                    value={dateFrom}
+                                    onChange={(e) => setDateFrom(e.target.value)}
+                                    className="mb-2 cursor-pointer bg-white dark:bg-[#22303C] dark:text-[#eee] dark:accent-[#eee] border border-gray-300 outline-0 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+                                    id="dateFrom"
+                                />
+                            </span>
+                            <span>
+                                <label htmlFor="dateTo" className="sr-only">
+                                    Date To
+                                </label>
+                                <input
+                                    type="date"
+                                    name="dateTo"
+                                    min={formatDate(minDate)}
+                                    max={formatDate(today)}
+                                    value={dateTo}
+                                    onChange={(e) => setDateTo(e.target.value)}
+                                    className="mb-2 cursor-pointer bg-white dark:bg-[#22303C] dark:text-[#eee] dark:accent-[#eee] border border-gray-300 outline-0 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+                                    id="dateTo"
+                                />
+                            </span>
+                        </div>
+                        <span>
+                            <label
+                                htmlFor="engineerFilter"
+                                className="block mb-2 text-sm font-medium  text-gray-900 dark:text-[#eee] sr-only"
+                            >
+                                department filter
+                            </label>
+                            <select
+                                value={department}
+                                onChange={(e) => setDepartment(e.target.value)}
+                                id="department"
+                                className="mb-2 bg-white border cursor-pointer border-gray-300 outline-0 dark:bg-[#22303C] dark:text-[#eee] text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            >
+                                <option disabled value="">
+                                    Filter by department
+                                </option>
+                                {gspnDeps.map((i) => (
+                                    <option key={i.id} value={i.departmentCode}>
+                                        {i.departmentName}
+                                    </option>
+                                ))}
 
-                        <option value={`HHP`}>
-                            HHP
-                        </option>
-                        <option value={`VDE`}>
-                            TVs
-                        </option>
-                        <option value={`REF`}>
-                            Fridges
-                        </option>
-                        <option value={`WSM`}>
-                            Washing machines
-                        </option>
-                        <option value={`HKE`}>
-                            Home kitchen electronics (dishwashers etc.)
-                        </option>
-                        <option value={`MON`}>
-                            Monitors
-                        </option>
-                    </select>
-                </span>
-            </div>
-            {/* {filteredData?.map((x) => (
-            <p key={x?.SvcOrderNo}>{x?.SvcOrderNo} - {x?.SvcProduct} -  {x?.EngineerName}</p>
-          ))} */}
+                            </select>
+                        </span>
+                    </div>
 
-            {/* 
-          
-          - in warranty
-          - out of warranty
-          - pending
-          - completed
-          - goods delivered
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+                        {
+                            displayCards.map((card) => (
+                                <DashboardCard
+                                    onClick={() => handleCardClick(card)}
+                                    key={card.id}
+                                    title={card.title}
+                                    paragraph={card.value}
+                                />
+                            ))
+                        }
+                    </div>
 
-        //   On click
+                    {/* Open this modal upon card click, it will open the card details matching specific card */}
+                    <Modal title="" isVisible={isModalOpen} onClose={closeModal} content={<>
 
-        -  service order
-        - engineer
-        - department
-        - fault
-        - imei
-        - serial number
-        - model number
-          
-          
-          
-          
-          */}
-            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3">
+                        {isModalOpen && (
 
-                <DashboardCard title='In warranty' paragraph={inWarrantyCount} />
-                <DashboardCard title='Out warranty' paragraph={outOfWarrantyCount} />
-                <DashboardCard title='Pending' paragraph={pendingCount} />
-                <DashboardCard title='Completed' paragraph={completedCount} />
-                <DashboardCard title='Goods delivered' paragraph={goodsDeliveredCount} />
+                            <div className="p-3 flex flex-col gap-2">
+                                {isModalOpen?.details?.map((i: any) => (
+                                    <div key={i?.SvcOrderNo} className="border-b">
+                                        <p>SvcOrderNo: {i?.SvcOrderNo}</p>
+                                        <p>Date: {moment(i.ReqDate).format('YYYY-MM-DD')}</p>
+                                        <p>SvcProduct: {i.SvcProduct}</p>
+                                        <p>Status: {i.StatusDesc}</p>
+                                        <p>Model: {i.Model}</p>
+                                    </div>
+                                ))}
+                                {/* <p>{isModalOpen?.details}</p> */}
+                            </div>
 
-                {/* <Modal
-                    isVisible={openModal}
-                    title={""}
-                    content={<>
+                        )}
 
-                        {countPending}
+                    </>} />
 
-
-                    </>}
-                    onClose={closeModal}
-                /> */}
-
-            </div>
-            <ToTopButton />
-
+                    {/* To top button */}
+                    <ToTopButton />
+                </>
+            )}
         </Container>
     );
 }
