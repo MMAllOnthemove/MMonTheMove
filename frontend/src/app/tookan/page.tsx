3eca9c59eea7eea231dca272c449ed4a8733101d
Katleho Mabala <katleho_m@allelectronics.co.za>
2024-10-21 11:00:42 +0200

    feat - create tookan tasks, and assign

---
+++ frontend/src/app/tookan/page.tsx
@@ -0,0 +1,10 @@
+import TookanScreen from '@/components/screens/tookan/page'
+import React from 'react'
+
+const Tookan = () => {
+    return (
+        <TookanScreen />
+    )
+}
+
+export default Tookan
--- frontend/src/lib/interfaces.ts
+++ frontend/src/lib/interfaces.ts
@@ -14,5 +14,30 @@ export interface IForgotPasswordFormValues {
     email: string;
 }
 export interface IContainer {
-      children: React.ReactNode;
+    children: React.ReactNode;
+}
+
+export interface ITookanCreateTask {
+    customer_email: string;
+    order_id: string;
+    customer_username: string;
+    customer_phone: string | number;
+    customer_address: string;
+    job_description: string;
+    job_pickup_datetime: string;
+    job_delivery_datetime: string;
+    job_pickup_latitude: string | number;
+    job_pickup_longitude: string | number;
+    has_pickup: string;
+    has_delivery: string;
+    layout_type: string;
+    tracking_link: number;
+    timezone: string;
+    api_key: string;
+}
+export interface ITookanAssignToTeam {
+    job_id: string;
+    team_id: string;
+    job_status: string;
+    api_key: string;
 }
