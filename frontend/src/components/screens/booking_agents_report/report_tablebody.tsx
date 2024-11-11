5c00a12b1d5b99dbaa250f298887cc6a71f733c8
Katleho Mabala <katleho_m@allelectronics.co.za>
2024-11-11 16:44:25 +0200

    chore - refactor booking reports

---
+++ frontend/src/components/screens/booking_agents_report/report_tablebody.tsx
@@ -0,0 +1,22 @@
+import { TAgentTasks } from '@/lib/types'
+import React from 'react'
+
+type TReportTableBody = {
+    groupedTasks: TAgentTasks[]
+    handleRowClick: (row: any) => void
+}
+const ReportTableBody = ({ groupedTasks, handleRowClick }: TReportTableBody) => {
+    return (
+        <tbody className="z-0">
+
+            {groupedTasks?.map((item: any) => (
+                <tr onClick={() => handleRowClick(item)} key={item.createdBy} className="border-b cursor-pointer hover:bg-gray-100">
+                    <td className="px-4 py-3 font-medium text-sm">{item.createdBy}</td>
+                    <td className="px-4 py-3 font-medium text-sm">{item.tasksCount}</td>
+                </tr>
+            ))}
+        </tbody>
+    )
+}
+
+export default ReportTableBody
